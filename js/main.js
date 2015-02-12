4044f8003440b57caf29430df8c0a072acfc5971
Christian-Gibbons <cgibbons@gmu.edu>
2015-02-12 00:24:21 -0500

    tileset attemp 1

--- js/main.js
+++ js/main.js
@@ -13,40 +13,49 @@ window.onload = function() {
     
     "use strict";
     
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create });
+
+function preload() {
+
+    //  Tilemaps are split into two parts: The actual map data (usually stored in a CSV or JSON file) 
+    //  and the tileset/s used to render the map.
+
+    //  Here we'll load the tilemap data. The first parameter is a unique key for the map data.
+
+    //  The second is a URL to the JSON file the map data is stored in. This is actually optional, you can pass the JSON object as the 3rd
+    //  parameter if you already have it loaded (maybe via a 3rd party source or pre-generated). In which case pass 'null' as the URL and
+    //  the JSON object as the 3rd parameter.
+
+    //  The final one tells Phaser the foramt of the map data, in this case it's a JSON file exported from the Tiled map editor.
+    //  This could be Phaser.Tilemap.CSV too.
+
+    game.load.tilemap('soviet', 'assets/tilemaps/maps/soviet.json', null, Phaser.Tilemap.TILED_JSON);
+
+    //  Next we load the tileset. This is just an image, loaded in via the normal way we load images:
+
+    game.load.image('tiles', 'assets/tilemaps/tiles/PostSovietTile.png');
+
+}
+
+var map;
+var layer;
+
+function create() {
+
+    game.stage.backgroundColor = '#787878';
+
+    //  The 'mario' key here is the Loader key given in game.load.tilemap
+    map = game.add.tilemap('soviet');
+
+    //  The first parameter is the tileset name, as specified in the Tiled map editor (and in the tilemap json file)
+    //  The second parameter maps this name to the Phaser.Cache key 'tiles'
+    map.addTilesetImage('SuperMarioBros-World1-1', 'tiles');
     
-    function preload() {
-        // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
-    }
-    
-    var bouncy;
-    
-    function create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
-        
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
-        
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something awesome.", style );
-        text.anchor.setTo( 0.5, 0.0 );
-    }
-    
-    function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
-    }
-};
+    //  Creates a layer from the World1 layer in the map data.
+    //  A Layer is effectively like a Phaser.Sprite, so is added to the display list.
+    layer = map.createLayer('World1');
+
+    //  This resizes the game world to match the layer dimensions
+    layer.resizeWorld();
+
+}
